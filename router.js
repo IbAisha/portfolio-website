7384df96f3cb824f7b36e113f6d9a0e8de489dc2
Aïsha <zawjatu.a@gmail.com>
2025-04-24 10:49:57 +0200

    npm upgrade

--- router.js
+++ router.js
@@ -1,24 +1,5 @@
-function loadPage(page) {
-    const content = document.getElementById('content');
-    
-    switch (page) {
-        case 'home':
-            content.innerHTML = '<h1>Bienvenue sur la page d\'accueil</h1>';
-            break;
-        case 'about':
-            content.innerHTML = '<h1>À propos de nous</h1>';
-            break;
-        default:
-            content.innerHTML = '<h1>Page non trouvée</h1>';
-    }
+async function loadPage(page) {
+    const response = await fetch(page);
+    const html = await response.text();
+    document.body.textContent = html; // Remplace TOUT le contenu
 }
-
-
-window.addEventListener('hashchange', () => {
-    const hash = window.location.hash.substring(2); // Enlève '#/'
-    loadPage(hash || 'home');
-});
-
-// Charger la page initiale
-loadPage(window.location.hash.substring(2) || 'home');
-
